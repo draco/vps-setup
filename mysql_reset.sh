cfcd39444b75127b40d4ca716405c9314355158c
Sunny Wong <kodeiko@gmail.com>
2014-06-11 00:53:51 +0800

    Mirror the new MySQL setup

--- mysql_reset.sh
+++ mysql_reset.sh
@@ -1,11 +1,11 @@
 cd ~
-new_password=$(perl -le 'print map { (a..z,A..Z,0..9)[rand 62] } 0..pop' 36)
-sudo service mysql stop
-echo "UPDATE mysql.user SET Password=PASSWORD('${new_password}') WHERE User='root';" > ~/rootpassword.sql
+MYSQL_ROOT_PASSWORD=$(perl -le 'print map { (a..z,A..Z,0..9)[rand 62] } 0..pop' 36)
+sudo /etc/init.d/mysql stop
+echo "UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User='root';" > rootpassword.sql
 echo "FLUSH PRIVILEGES;" >> rootpassword.sql
 sudo mysqld_safe --init-file=${HOME}/rootpassword.sql &
-sudo service mysql restart
-sudo rm rootpassword.sql
+expect -c "
+spawn mysql_config_editor set --login-path=root --host=localhost --user=root --password
+expect -nocase \"Enter password:\" {send \"$MYSQL_ROOT_PASSWORD\r\"; interact}
+"
 cd -
-
-echo "Root MySQL Password: $new_password"
