6f1576987222c82f734ca4ccae9e8e49edbd80a8
Sunny Wong <kodeiko@gmail.com>
2014-06-11 00:50:25 +0800

    Use --login-path to reduce use of password for root mysql

--- setup.sh
+++ setup.sh
@@ -52,7 +52,14 @@ sudo /etc/init.d/mysql stop
 echo "UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User='root';" > rootpassword.sql
 echo "FLUSH PRIVILEGES;" >> rootpassword.sql
 sudo mysqld_safe --init-file=${HOME}/rootpassword.sql &
+expect -c "
+spawn mysql_config_editor set --login-path=root --host=localhost --user=root --password
+expect -nocase \"Enter password:\" {send \"$MYSQL_ROOT_PASSWORD\r\"; interact}
+"
 cd -
+
+sudo /etc/init.d/mysql stop
+sudo /etc/init.d/mysql start
 
 ###----------------------------------------###
 ###  Configure Nginx
