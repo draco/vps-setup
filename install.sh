0ed5457b299eb97ed27cf272638c03932048755a
Sunny Wong <kodeiko@gmail.com>
2014-06-14 15:24:29 +0800

    Test install 3

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd ~
 wget --no-check-certificate -O debian.tar.gz https://github.com/draco/vps-setup/tarball/debian-mysql
 tar -zxvf debian.tar.gz
-sudo cd **vps-setup**
+sudo cd *vps-setup*
 sudo sh setup.sh
